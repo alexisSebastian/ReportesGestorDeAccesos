a364cb2ae4e3c43df053cd409a760148a2fe819e
Alexis Ramos <alexisramoscaballero@gmail.com>
2019-01-16 18:41:38 -0600

    se agrega materialize y demas componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { CuentasSiNUsoComponent } from './pages/cuentas-si-nuso/cuentas-si-nuso.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent,
+    LoginComponent,
+    CuentasSiNUsoComponent,
+    HeaderComponent,
+    SidenavComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
